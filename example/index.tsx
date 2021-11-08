9f3cbe6728fe37e96d89a2b33300bb0bef1aaf62
bang88 <sqibang@gmail.com>
2021-11-08 11:38:52 +0800

    refactor: example

--- example/index.tsx
+++ example/index.tsx
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native'
 import { name as appName } from './app.json'
-import App from './src/CustomThemeExample'
+import App from './src/index'
 
 AppRegistry.registerComponent(appName, () => App)
